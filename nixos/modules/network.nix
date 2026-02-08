9dc19cd987634a47b9ad6a97082d3be5b02cb309
ma2sakura <sakura@ma2.lol>
2026-02-08 23:50:55 +0000

    build: remove ipsec/strongswan config

--- nixos/modules/network.nix
+++ nixos/modules/network.nix
@@ -12,22 +12,4 @@
   networking.hosts = {
     "10.129.13.188" = [ "facts.htb" ];
   };
-
-  # # Create writable /etc/ipsec.d for nm-l2tp secrets
-  # systemd.tmpfiles.rules = [
-  #   "d /etc/ipsec.d 0755 root root -"
-  # ];
-
-  # # Strongswan config for nm-l2tp
-  # environment.etc."strongswan.conf".text = ''
-  #   charon {
-  #     load_modular = no
-  #     plugins {
-  #       include ${pkgs.strongswan}/etc/strongswan.d/charon/*.conf
-  #     }
-  #   }
-  #   libstrongswan {
-  #     integrity_test = no
-  #   }
-  # '';
 }
